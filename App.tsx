2fffea02bac1648b38cd559f317d90cc9f87417e
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-08-08 07:45:38 +0100

    Splashscreen layout styling added

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ export default function App() {
         <TextInput style={styles.textInput} placeholder='Your next goal' />
         <Button title='Add Goal' />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals</Text>
       </View>
     </View>
@@ -18,16 +18,26 @@ export default function App() {
 const styles = StyleSheet.create({
   appContainer: {
     padding: 50,
+    paddingHorizontal: 16,
+    flex: 1,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
     gap: 8,
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: "80%",
+    width: "70%",
     padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
   }
 });
